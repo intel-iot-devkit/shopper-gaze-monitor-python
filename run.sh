383f588fc56b4f5baf4f86cb0e8a2320833b6739
izaidi7-testing <69081109+izaidi7-testing@users.noreply.github.com>
2021-08-26 21:11:02 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-cd ../application
-python3 shopper_gaze_monitor.py -m ../resources/face-detection-adas-0001.xml -pm ../resources/head-pose-estimation-adas-0001.xml -d CPU
+cd application/
+python3 shopper_gaze_monitor.py -m resources/face-detection-adas-0001.xml -pm resources/head-pose-estimation-adas-0001.xml -d CPU
