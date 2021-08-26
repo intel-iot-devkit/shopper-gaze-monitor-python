7b8b59ebcec048a0d8a68c7bf34b0d02d720e47b
izaidi7-testing <69081109+izaidi7-testing@users.noreply.github.com>
2021-08-26 21:01:28 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python3 shopper_gaze_monitor.py -m /opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/intel/face-detection-adas-0001/FP32/face-detection-adas-0001.xml -pm /opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/intel/head-pose-estimation-adas-0001/FP32/head-pose-estimation-adas-0001.xml -d CPU
+cd ../application
+python3 shopper_gaze_monitor.py -m ../resources/face-detection-adas-0001.xml -pm ../resources/head-pose-estimation-adas-0001.xml -d CPU
